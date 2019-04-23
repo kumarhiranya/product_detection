af3bd0af0e5e518eeb8c80a5017a4ee30ee517d2
kumarhiranya <khiranyag@gmail.com>
2019-04-22 19:53:10 -0500

    Changes to image_rename and image filenames.

--- image_rename.py
+++ image_rename.py
@@ -7,17 +7,23 @@ imageExtensions = 'png jpg jpeg'.split()
 # extensions = pd.Series([x.split('.')[-1] for x in os.listdir(basePath)])
 # print(extensions.unique())
 
-files = [x for x in os.listdir(basePath) if x.split('.')[-1].lower() in imageExtensions]
+files = [(x[:-len(x.split('.')[-1])-1], x.split('.')[-1]) for x in os.listdir(basePath) if x.split('.')[-1].lower() in imageExtensions]
+existingNumbers = {int(x[0]):x[1] for x in files if x[0].isdigit()}
 
-existingNumbers = {}
-for i,f in enumerate(files):
-    try:
-        x = int(f.split('.')[0])
-        existingNumbers[x] = 0
-    except:
-        c=i
-        while c in existingNumbers.keys():
-            c+=1
-        os.rename(basePath+f, basePath+str(c)+'.'+str(f.split('.')[-1]))
-
-print('renamed %d files.', i)
+i=0
+cnt=0
+for f in files:
+    rename = 0
+    if f[0].isdigit():
+        if int(f[0])>10000:
+            print(f[0], 'is an integer above 10000, renaming file....')
+            rename = 1
+    else:
+        print(f[0], 'is not an integer, renaming file....')
+        rename = 1
+    if rename:
+        while os.path.exists(basePath+str(i)+'.'+f[1]):
+            i+=1
+        os.rename(basePath+f[0]+'.'+f[1], basePath+str(i)+'.'+f[1])
+        cnt+=1
+print('Renamed', cnt, 'files.')
